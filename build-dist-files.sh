252947a345425eb5c980aa4358926a48a5d1b618
Tim Harder <radhermit@gmail.com>
2017-09-22 19:43:33 -0400

    dist: add support for doing test runs via API-requested builds

--- build-dist-files.sh
+++ build-dist-files.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
 # Build and upload dist files to pypi on travis-ci.
 #
-# Note that this assumes TWINE_USERNAME and TWINE_PASSWORD are securely exported in
-# order for twine to work properly.
+# Note that deploying to pypi assumes TWINE_USERNAME and TWINE_PASSWORD are
+# securely exported to the environment in order for twine to work properly.
 
 set -ev
 
-if [[ ${TRAVIS_PYTHON_VERSION} == "3.6" ]] && [[ -n ${TRAVIS_TAG} ]]; then
-	# create sdist
-	pip install -r requirements/dist.txt
-	python setup.py sdist
+# either we're running for a release or a explicitly requested build via API
+if [[ -n ${TRAVIS_TAG} ]] || [[ ${TRAVIS_EVENT_TYPE} == api ]]; then
+	if [[ ${TRAVIS_PYTHON_VERSION} == "3.6" ]]; then
+		# create sdist
+		pip install -r requirements/dist.txt
+		python setup.py sdist
 
-	# create wheels
-	cibuildwheel --output-dir dist
+		# create wheels
+		cibuildwheel --output-dir dist
 
-	# show the produced dist files
-	ls dist/
-	tar -ztf dist/*.tar.gz | sort
+		# show the produced dist files
+		ls dist/
+		tar -ztf dist/*.tar.gz | sort
 
-	# upload dist files to pypi
-	twine upload dist/*
-fi
+		# only deploy tagged releases
+		if [[ -n ${TRAVIS_TAG} ]]; then
+			# upload dist files to pypi
+			twine upload dist/*
+		fi
+	fi
 
 exit 0
